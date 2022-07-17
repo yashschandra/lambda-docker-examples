3ae1228112684dabd714ae63e02c9a5a23277a9a
yashcfg <yash@cloudfeather.games>
2022-07-18 00:30:15 +0530

    exception handling

--- python/app/app.py
+++ python/app/app.py
@@ -3,7 +3,14 @@ import boto3
 
 def upload_file(local_file, s3_file):
     s3 = boto3.client('s3')
-    s3.upload_file(local_file, 'bounty-public', s3_file)
+    try:
+        s3.upload_file(local_file, 'bounty-public', s3_file)
+    except FileNotFoundError:
+        print("The file was not found")
+    except NoCredentialsError:
+        print("Credentials not available")
+    except e:
+        print('exception ', e)
 
 
 def handler(event, context):
