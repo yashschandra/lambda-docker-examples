09a71973a02e86204d176630f26e3c0735914834
yashcfg <yash@cloudfeather.games>
2022-07-17 23:00:37 +0530

    added html2image support in dockerfile and app

--- python/app/app.py
+++ python/app/app.py
@@ -1,2 +1,7 @@
+from html2image import Html2Image
+
 def handler(event, context):
-    return "Hello World!"
+    print(event)
+    hti = Html2Image()
+    print(hti.browser.flags)
+    return "it works"
