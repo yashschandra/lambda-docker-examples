557d84cfaf5502e2f619c34744e514f4552172e0
yashcfg <yash@cloudfeather.games>
2022-07-18 00:14:33 +0530

    added boto3 to upload to s3

--- python/app/app.py
+++ python/app/app.py
@@ -1,7 +1,14 @@
 from html2image import Html2Image
+import boto3
+
+def upload_file(local_file, s3_file):
+    s3 = boto3.client('s3')
+    s3.upload_file(local_file, 'bounty-public', s3_file)
+
 
 def handler(event, context):
     print(event)
-    hti = Html2Image()
-    print(hti.browser.flags)
+    hti = Html2Image(output_path='/tmp')
+    hti.screenshot(html_str=event['html_str'], save_as='dummy.png')
+    upload_file('/tmp/bounty.png', 'posters/test/dummy.png')
     return "it works"
