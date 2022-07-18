bdb27f45163bdf053bc573f19ac7b2d93737037c
yashcfg <yash@cloudfeather.games>
2022-07-18 18:12:07 +0530

    trying with webshot and wkhtmltopdf

--- python/app/app.py
+++ python/app/app.py
@@ -1,2 +1,11 @@
+from htmlwebshot import WebShot
+import boto3
+
 def handler(event, context):
+    html = event['html_str']
+    shot = WebShot()
+    shot.create_pic(html=html, output="/tmp/picture.jpg")
+    s3 = boto3.resource('s3')
+    data = open('/tmp/picture.jpg', 'rb')
+    s3.Bucket('bounty-public').put_object(Key='posters/test/picture.jpg', Body=data)
     return "Hello World!"
