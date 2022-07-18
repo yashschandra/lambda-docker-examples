93bbebec548e5842a021452c114880f10ea28b49
yashcfg <yash@cloudfeather.games>
2022-07-18 18:49:02 +0530

    write html file before taking shot

--- python/app/app.py
+++ python/app/app.py
@@ -2,9 +2,10 @@ from htmlwebshot import WebShot
 import boto3
 
 def handler(event, context):
-    html = event['html_str']
     shot = WebShot()
-    shot.create_pic(html=html, output="/tmp/picture.jpg")
+    with open('/tmp/content.html', 'w+') as htmlfile:
+        htmlfile.write(event['html_str'])
+    shot.create_pic(html='/tmp/content.html', output="/tmp/picture.jpg")
     s3 = boto3.resource('s3')
     data = open('/tmp/picture.jpg', 'rb')
     s3.Bucket('bounty-public').put_object(Key='posters/test/picture.jpg', Body=data)
