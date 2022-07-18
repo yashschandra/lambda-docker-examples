4a014570804c7731ec43d2c8eadf8ef75f5c2420
yashcfg <yash@cloudfeather.games>
2022-07-18 15:11:02 +0530

    added debug logs

--- python/app/app.py
+++ python/app/app.py
@@ -16,10 +16,18 @@ def handler(event, context):
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-dev-tools")
     options.add_argument("--no-zygote")
-    options.add_argument("--remote-debugging-port=9222")
     driver = webdriver.Chrome("/opt/chromedriver",
                               options=options)
-    driver.get("data:text/html;charset=utf-8," + event['html_str'])
-    with io.BytesIO(driver.get_screenshot_as_png()) as f:
-        s3.upload_fileobj(f, 'bounty-public', 'posters/test/screenshot.png')
+    print('driver opened')
+    try:
+        driver.get("data:text/html;charset=utf-8," + event['html_str'])
+        print('driver got, upload...')
+    except e:
+        print('driver did not get ', e)
+    try:
+        with io.BytesIO(driver.get_screenshot_as_png()) as f:
+            s3.upload_fileobj(f, 'bounty-public', 'posters/test/screenshot.png')
+            print('uploaded')
+    except e:
+        print('not able to upload ', e)
     driver.close()
