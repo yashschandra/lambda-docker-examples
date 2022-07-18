7179181026134779580fcfe9ed5efe84ec2bec78
yashcfg <yash@cloudfeather.games>
2022-07-18 18:54:24 +0530

    debug logs added

--- python/app/app.py
+++ python/app/app.py
@@ -2,11 +2,16 @@ from htmlwebshot import WebShot
 import boto3
 
 def handler(event, context):
+    print(event['html_str'])
     shot = WebShot()
+    print('writing to local file')
     with open('/tmp/content.html', 'w+') as htmlfile:
         htmlfile.write(event['html_str'])
+    print('wrote local file, taking shot')
     shot.create_pic(html='/tmp/content.html', output="/tmp/picture.jpg")
+    print('took shot, uploading to s3')
     s3 = boto3.resource('s3')
     data = open('/tmp/picture.jpg', 'rb')
     s3.Bucket('bounty-public').put_object(Key='posters/test/picture.jpg', Body=data)
+    print('uploaded')
     return "Hello World!"
